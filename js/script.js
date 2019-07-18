fa77a6a72825ac02a036f67d9d7aa7269d4a2b21
Sebastien La Duca <sladuca777@gmail.com>
2019-07-18 00:22:24 -0400

    init blog sidenav

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
   $(document).ready(function(){
     $('.path').each(function() {
-    	$(this).css("stroke-dasharray", this.getTotalLength());
+      $(this).css("stroke-dasharray", this.getTotalLength());
     	$(this).css("stroke-dashoffset", this.getTotalLength())
     });
     $('.scrollspy').scrollSpy({
-    	scrollOffset: 200
-    	});
+      scrollOffset: 200
+    });
     $('.table-of-contents').pushpin({
-		// push below navbar
-		offset: 300
-	});
+      // push below navbar
+      offset: 300
+	  });
     $('.fixed-action-btn').floatingActionButton();
     $('.tooltipped').tooltip();
     $('.collapsible').collapsible({
     	accordion: false
+    });
+    $('.sidenav').sidenav({
+      edge: 'right'
     });
   });
 
